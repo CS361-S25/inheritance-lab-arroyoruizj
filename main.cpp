e823ba453a54caea13a64e6bc121e76570f6d929
arroyoruizj <arroyoruizj@carleton.edu>
2025-04-23 11:14:47 -0500

    almost done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,19 @@
-
 #include "emp/base/vector.hpp"
 #include "emp/base/Ptr.hpp"
 #include <ostream>
-
+#include "Animal.h"
+#include "Dog.h"
 
 int main() {
     
+    emp::vector<emp::Ptr<Animal>> population;
+
+    population.push_back(new Dog(5, 2));
+    std::cout << population[0]->GetType() << std::endl;
+    std::cout << population[0]->GetAge() << std::endl;
+    std::cout << population[0].DynamicCast<Dog>()->GetSpots() << std::endl;
+
+    population.push_back(population[0]->Reproduce());
+    std::cout << population[1]->GetType() << std::endl;
 
 }
